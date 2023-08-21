d2beb593cd4c02126971245c01c650103b2ed52c
Onni <onni.luova@gmail.com>
2023-08-21 14:13:48 +0300

    ekan directory vaihdettu

--- Moduuli2/2.2.py
+++ Moduuli2/2.2.py
@@ -1,3 +1,3 @@
 from math import pi
-r = float(input ("Lisää ympyrän säde: "))
-print ("Ympyrän säde " + str(r) + " on: " + str(pi * r**2))
+r = float(input("Lisää ympyrän säde: "))
+print("Ympyrän säde " + str(r) + " on: " + str(pi * r**2))
