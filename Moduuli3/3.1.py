9198e80af96384e50adfc3c3be4c27c08e70d119
Onni <onni.luova@gmail.com>
2023-08-22 21:09:24 +0300

    3.1 tehty

---
+++ Moduuli3/3.1.py
@@ -0,0 +1,7 @@
+kuha = int(input("Kuhan pituus: "))
+raja = int(37)
+
+if kuha < 37:
+    print("Kuhasi on alamittainen. Laske se takasin veteen. Kuhan pituudesta puuttuu: " + str(raja - kuha) + " cm.")
+else:
+    print("Hyvän mittainen kuha!")
