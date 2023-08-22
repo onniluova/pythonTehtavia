31befe83ecb06f12c84c4abb5e3f073366a8ecdc
Onni <onni.luova@gmail.com>
2023-08-22 08:58:15 +0300

    ekan directory vaihdettu

---
+++ Moduuli2/2.3.py
@@ -0,0 +1,5 @@
+kanta = float(input("Lisää suorakulmion kanta: "))
+korkeus = float(input("Lisää suorakulmion korkeus: "))
+
+print("Piiri: " + str(korkeus + kanta * 2))
+print("Pinta ala: " + str(korkeus * kanta))
