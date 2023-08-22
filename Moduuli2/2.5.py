e50836112d82be7b72858cb5c536d0c10e85cc49
Onni <onni.luova@gmail.com>
2023-08-22 10:14:07 +0300

    2.4 valmis

---
+++ Moduuli2/2.5.py
@@ -0,0 +1,20 @@
+leiviska = int(8504)
+naula = int(425)
+luoti = int(13)
+summa = int(0)
+kilo = int(0)
+
+leiviskat = int(input("Anna leiviskat: " ))
+naulat = int(input("Anna naulat: " ))
+luodit = int(input("Anna luodit: " ))
+
+summa = leiviskat * leiviska + naula * naulat + luoti * luodit
+
+while summa > 1000:
+    kilo+=1
+    summa-=1000
+
+print(str(kilo) + " kilogrammaa ja " + str(summa) + " grammaa.")
+
+
+
