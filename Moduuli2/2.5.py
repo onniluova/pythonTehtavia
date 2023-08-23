b73c763a2e471ac26034847a6e7ffe7c65b24ac9
Onni <onni.luova@gmail.com>
2023-08-23 09:56:28 +0300

    3.4 valmis

--- Moduuli2/2.5.py
+++ Moduuli2/2.5.py
@@ -3,6 +3,7 @@ naula = int(425)
 luoti = int(13)
 summa = int(0)
 kilo = int(0)
+gramma = int(0)
 
 leiviskat = int(input("Anna leiviskat: " ))
 naulat = int(input("Anna naulat: " ))
@@ -10,10 +11,10 @@ luodit = int(input("Anna luodit: " ))
 
 summa = leiviskat * leiviska + naula * naulat + luoti * luodit
 
-while summa > 1000:
-    kilo+=1
-    summa-=1000
+kilo = summa / 1000
+gramma = summa % 1000
+kilo = int(kilo)
 
-print("Massa nykymittojen mukaan " + str(kilo) + " kilogrammaa ja " + str(summa) + " grammaa.")
+print("Massa nykymittojen mukaan " + str(kilo) + " kilogrammaa ja " + str(gramma) + " grammaa.")
 
 
