be04718fec12c1ec4f2d7be88e8a1fccb6f91b5f
Onni <onni.luova@gmail.com>
2023-08-23 22:19:33 +0300

    4.3 toimii

--- Moduuli4/4.3.py
+++ Moduuli4/4.3.py
@@ -1,5 +1,5 @@
 num = int(0)
-pienin = int(0)
+pienin = int(-1)
 suurin = int(0)
 syote = "0"
 
@@ -9,7 +9,7 @@ while syote != "":
         num = int(syote)
         if num > suurin:
             suurin = num
-        elif num < pienin:
+        elif pienin == -1 or num < pienin:
             pienin = num
 
 print("Suurin numero: " + str(suurin) + " pienin numero: " + str(pienin))
