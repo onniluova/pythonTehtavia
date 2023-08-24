e1064333fee6e52f55f904c644f5121982763a16
Onni <onni.luova@gmail.com>
2023-08-24 14:21:53 +0300

    5.1 korjattu

--- Moduuli5/5.1.py
+++ Moduuli5/5.1.py
@@ -1,7 +1,12 @@
 import random
 
-arpakuutiot = [1, 1, 1, 1]
+arpakuutiot = []
+kysy = int(input("Syötä arpakuutioiden määrä: "))
 summa = int(0)
+
+while kysy > 0:
+    arpakuutiot.append(0)
+    kysy-=1
 
 for a in arpakuutiot:
     a = random.randint(1,6)
