fa69c3bcbb273017e10daa1d101bb64db4939389
Onni <onni.luova@gmail.com>
2023-08-28 20:24:36 +0300

    6.6 toimii ja commitattu

--- Moduuli6/6.6.py
+++ Moduuli6/6.6.py
@@ -1,9 +1,28 @@
 class pizza:
-    hinta = int(10)
-    halkasija = int(120)
-def palautaPizza (halkaisija, hinta):
-    neliometri = int(halkaisija * 10000)
-    hinta = int(hinta / neliometri)
+    def __init__(self, hinta, halkaisija):
+        self.hinta = hinta
+        self.halkaisija = halkaisija
+
+def pizzanNelioHinta (self):
+    neliometri = float(self.halkaisija % 10000)
+    hinta = float(self.hinta / neliometri)
     return hinta
 
-palautaPizza(pizza)
+#Alla olevat olisi hyvä olla erillisessä tiedostossa selkeyden vuoksi
+pizza1hinta = int(input("Syötä pizza 1 hinta: "))
+pizza1halkaisija = int(input("Syötä pizza 1 halkaisija: "))
+
+pizza2hinta = int(input("Syötä pizza 2 hinta: "))
+pizza2halkaisija = int(input("Syötä pizza 2 halkaisija: "))
+
+p1 = pizza(pizza1hinta, pizza1halkaisija)
+p2 = pizza(pizza2hinta, pizza2halkaisija)
+
+if pizzanNelioHinta(p1) < pizzanNelioHinta(p2):
+    print("Pizzan 1 hinta: " + str(pizzanNelioHinta(p1)) + " euroa per neliömetri ja parempi hintalaatusuhteeltaan.")
+
+elif pizzanNelioHinta(p1) == pizzanNelioHinta(p2):
+    print("Pizzat ovat samanhintaiset!")
+
+else:
+    print("Pizzan 2 hinta: " + str(pizzanNelioHinta(p2)) + " euroa per neliömetri ja parempi hintalaatusuhteeltaan.")
