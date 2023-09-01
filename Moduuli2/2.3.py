a03abd2c12aa13a6a1b24b4807db16e40ba91958
Onni <onni.luova@gmail.com>
2023-09-01 13:16:55 +0300

    2.3 muutettu

--- Moduuli2/2.3.py
+++ Moduuli2/2.3.py
@@ -1,5 +1,5 @@
 kanta = float(input("Lisää suorakulmion kanta: "))
 korkeus = float(input("Lisää suorakulmion korkeus: "))
 
-print("Piiri: " + str(korkeus + kanta * 2))
+print("Piiri: " + str((korkeus + kanta) * 2))
 print("Pinta ala: " + str(korkeus * kanta))
